06ff97d3d9a5ef055720d274c3e926e154c84406
honger <honger05@126.com>
2016-07-01 01:31:34 +0800

    f

--- src/modules/animatediv/animatediv.js
+++ src/modules/animatediv/animatediv.js
@@ -5,32 +5,79 @@ let btn = document.getElementById('btn')
 
 btn.onclick = function () {
   if (container.offsetLeft === 0) {
-    move(container, 'left', -200)
+    move(container, {'height': 100, 'opacity': 100}, function () {
+      move(container, {'left': -200})
+    })
   } else {
-    move(container, 'left', 0)
+    move(container, {'left': 0}, function () {
+      move(container, {'height': 300, 'opacity': 20})
+    })
   }
 }
 
 let timer = null
+let isComplete = false
 
-function move (obj, attr, target, fn) {
+/**
+ * 动画函数
+ *
+ * @param  {Element}   obj       [执行动画的元素]
+ * @param  {Object}   attrClass  [执行动画的样式属性]
+ * @param  {Function} fn        [动画执行完成后的回调函数]
+ */
+function move (obj, attrClass, fn) {
   clearInterval(timer)
   timer = setInterval(() => {
-    var speed = (target - getStyle(obj, attr)) / 10
-    speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed)
-    if (getStyle(obj, attr) === target) {
+    for (let attr in attrClass) {
+      let curStyle = getStyle(obj, attr)
+      let tagStyle = attrClass[attr]
+
+      if (curStyle === tagStyle) {
+        isComplete = true
+        continue
+      }
+      isComplete = false
+
+      let speed = (tagStyle - curStyle) / 20
+      speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed)
+
+      setStyle(obj, attr, (curStyle + speed))
+      console.log(attr, obj.style[attr])
+    }
+
+    if (isComplete) {
       clearInterval(timer)
       fn && fn()
-    } else {
-      obj.style[attr] = (getStyle(obj, attr) + speed) + 'px'
-      console.log(obj.style[attr])
     }
-  }, 30)
+  }, 16)
 }
 
+/**
+ * 获取元素样式
+ *
+ * @param  {Element} obj  [元素]
+ * @param  {String} attr [元素属性]
+ * @return {Float}      [元素属性值]
+ */
 function getStyle (obj, attr) {
+  let opacity = attr === 'opacity' ? 100 : 1
   if (obj.currentStyle) {
-    return parseFloat(obj.currentStyle[attr])
+    return parseFloat(obj.currentStyle[attr]) * opacity
   }
-  return parseFloat(getComputedStyle(obj, false)[attr])
+  return parseFloat(getComputedStyle(obj, false)[attr]) * opacity
+}
+
+/**
+ * 设置元素样式
+ *
+ * @param {Element} obj   [元素]
+ * @param {String} attr  [元素属性]
+ * @param {Float} style [元素属性值]
+ */
+function setStyle (obj, attr, style) {
+  if (attr === 'opacity') {
+    obj.style[attr] = style / 100.0
+  } else {
+    obj.style[attr] = style + 'px'
+  }
 }
